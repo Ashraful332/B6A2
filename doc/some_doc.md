2f4d02b661ad41450c84f25c4e7d627c4588e744
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-07 13:45:59 +0600

    registration program is complite

--- doc/some_doc.md
+++ doc/some_doc.md
@@ -1,3 +1,5 @@
 ## Some Doc i use on this project
 
 - express js : https://expressjs.com/
+
+- bcrypt : https://medium.com/@bhupendra_Maurya/password-hashing-using-bcrypt-e36f5c655e09
--- src/auth/signup.ts
+++ src/auth/signup.ts
@@ -1,8 +1,47 @@
 import express from "express";
 const router = express.Router();
+import db from '../DB/mysql.js';
+import bcrypt from 'bcrypt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-router.get('/signup',(req,res)=>{
+router.get('/signup', (req, res) => {
     res.status(404).send("Only Post method on signup api");
+})
+
+// crete new user or admin
+router.post('/signup', async (req, res) => {
+    const { name, email, password, phone, role } = req.body;
+    // console.log(name, email, password, phone, role );
+
+    try {
+        const saltRounds: number = Number(process.env.SALTROUNDS); // import salt rounds as number
+        const hashedPassword = await bcrypt.hash(password, saltRounds) // hash password 
+
+        const sql = 'INSERT INTO users (name, email, hashedPassword, phone, role ) VALUES (?,?,?,?,?) '
+        const values = [name, email, hashedPassword, phone, role];
+
+        // Execute the query using the connection pool
+        const [result]: any = await db.execute(sql, values);
+
+        console.log('Record inserted:', result.insertId);
+        res.status(2001).json({
+            "success": true,
+            "message": "User registered successfully",
+            "data": {
+                "id": result.insertId,
+                "name": name,
+                "email": email,
+                "phone": phone,
+                "role": role
+            }
+        })
+
+    } catch (error: any) {
+        console.error('Error inserting data:', error);
+        res.status(500).json({ message: 'Error inserting data', error: error.message });
+    }
+
 })
 
 export default router
