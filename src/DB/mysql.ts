d3ed3f865014e049f08b9ee9f43da24257ebe2ac
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-07 12:57:40 +0600

    connect mysql database

---
+++ src/DB/mysql.ts
@@ -0,0 +1,17 @@
+import mysql, { type Pool } from 'mysql2';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const connection : Pool = mysql.createPool({
+    host: `${process.env.MYSQL_HOST}`, // Your MySQL host
+    user: `${process.env.MYSQL_USER}`,      // Your MySQL username
+    password: `${process.env.MYSQL_PASSWORD}`, // Your MySQL password
+    database: `${process.env.MYSQL_DATABASE}`, // The database you want to use
+    waitForConnections: true,
+    connectionLimit: 10, // Recommended for larger applications
+    queueLimit: 0
+});
+
+// module.exports = connection;
+export default connection;
+
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 // src/index.ts
 import express from 'express';
-
+import * as dotenv from 'dotenv';
 const app = express();
 const port = 3000;
+import db from './DB/mysql.js';
+
+dotenv.config();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello TypeScript + Node.js!');
 });
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("DB connection error");
+  } else {
+    console.log("Database connected");
+    connection.release();
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
