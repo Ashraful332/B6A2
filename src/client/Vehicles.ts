2ba7f0777fcea8a216a6d9b27923a91b266dbbb6
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-09 07:44:48 +0600

    add vehicles

--- src/client/Vehicles.ts
+++ src/client/Vehicles.ts
@@ -6,6 +6,32 @@ dotenv.config();
 import authenticateToken from "../lib/Authorization.js"
 
 
-router.post("/")
+router.post("/", authenticateToken, async (req, res) => {
+    const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
+    try {
+        const sql = 'INSERT INTO Vehicles (vehicle_name,type,registration_number,daily_rent_price,availability_status) VALUES (?,?,?,?,?)'
+        const values = [vehicle_name, type, registration_number, daily_rent_price, availability_status]
+
+        // execute the query
+        const [result]: any = await db.execute(sql, values)
+        console.log(result);
+        
+        res.status(201).json({
+            "success": true,
+            "message": "Vehicle created successfully",
+            "data": {
+                "id": result.insertId,
+                "vehicle_name": vehicle_name,
+                "type": type,
+                "registration_number": registration_number,
+                "daily_rent_price": daily_rent_price,
+                "availability_status": availability_status
+            }
+        })
+    } catch (error: any) {
+        console.log("error is coming : ", error);
+        res.status(500).json({ message: "Error is coming", error: error.message })
+    }
+})
 
 export default router
